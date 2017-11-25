53821fb8532c86ad8f8aaa2fdc2d791f6d251baa
unknown <luhemeso@gmail.com>
2017-11-25 11:41:51 -0200

    hierarquia

---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('./views/includes/head.php'); ?>
+<body>
+    <h1>Sistema E-Commerce</h1>
+</body>
+</html>
--- views/includes/head.php
+++ views/includes/head.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="../assets/lib/jquery-3.2.1.min.js"></script>
+    <script src="../assets/script/login.js"></script>
+    <script src="../assets/script/dashboard.js"></script>
     <title>Document</title>
 </head>
-<body>
-    <h1>Sistema E-Commerce</h1>
-</body>
-</html>
