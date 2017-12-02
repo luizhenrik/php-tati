a0047d4af220b3ca2b7c8e12a8fa817d650b8a9c
unknown <luhemeso@gmail.com>
2017-12-02 18:42:38 -0200

    finalizando todo o carrinho

--- views/cart.php
+++ views/cart.php
@@ -13,50 +13,88 @@
     <?php include('includes/header.php'); ?>
     <main>
         <div class="container">
-            <div class="row">
                 <?php  
 
                     // Trazer Produtos do carrinho
-                    if(isset($_SESSION['test'])){
-                        //print_r($_SESSION['test']);
+                    // verifica se existe valor na variavel onde guardamos todos os produtos selecionados.
+                    if(isset($_SESSION['test'])):
+
+                        // Chamando Metodo da classe onde temos o SELECT
                         $cart_products= $products->getAllInArray('"' . implode('","', array_keys($_SESSION['test'])) . '"');
-                        if($cart_products->num_rows > 0){
-                            while($row = $cart_products->fetch_assoc()){
-                                echo 'Nome do produto: ' . "<br>" . $row['nome'] . "<br>";
 
-                                echo 'Valor do produto: ' . "<br>" . $row['valor'] . "<br>";
+                        // Verifica se contem produtos nessa resposta
+                        if($cart_products->num_rows > 0):
 
-                                echo 'QTD do produto: ' . "<br>" . $_SESSION['test'][$row['id']]['qtd_product'] . "<br>";
+                            // faz o laço para mostrar produtos
+                            while($row = $cart_products->fetch_assoc()):
+                ?>
+                                <p>
+                                    <strong>Nome do produto:</strong>
+                                    <br>
+                                    <?php echo $row['nome'];?>
+                                </p>
+                                <p>
+                                    <strong>Valor do produto:</strong>
+                                    <br>
+                                    <?php echo $row['valor'];?>
+                                </p>
+                                <p>
+                                    <strong>QTD do produto:</strong>
+                                    <br>
+                                    <?php echo $_SESSION['test'][$row['id']]['qtd_product'];?>
+                                </p>
+                                <p>
+                                    <strong>Valor total do produto:</strong>
+                                    <br>
+                                    <?php echo $row['valor'] * $_SESSION['test'][$row['id']]['qtd_product'];?>
+                                </p>
 
-                                echo 'Valor total do produto: ' . "<br>" . $row['valor'] * $_SESSION['test'][$row['id']]['qtd_product'] . "<br>";
+                                <a class='button-cart' href='../helpers/validate-cart.php' data-handler='add_product' data-id='<?php echo $row["id"] ?>'>Adicionar</a>
+                                <br>
+                                <a class='button-cart' href='../helpers/validate-cart.php' data-handler='remove_product' data-id='<?php echo $row["id"] ?>'>Retirar</a>
+                                <br>
+                                <a class='button-cart' href='../helpers/validate-cart.php' data-handler='del_product' data-id='<?php echo $row["id"] ?>'>Remover</a>
+                                
+                        <?php 
+                            endwhile; 
+                        ?>
+                        <br>
+                        <br>
+                        <br>
+                        <a class='button-cart--truncate' href='../helpers/validate-cart-truncate.php' data-handler='truncate_cart'>Limpar carrinho</a>
+                        <br>
+                <?php
+                        else:
+                ?>
+                        <h2>Seu carrinho esta vazio.</h2>
+                <?php
+                        endif;
 
-                                echo "<a class='button-cart' href='../helpers/validate-cart.php' data-handler='add_product' data-id='" . $row['id'] . "'>+</a>" . "<br>";
-
-                                echo "<a class='button-cart' href='../helpers/validate-cart.php' data-handler='remove_product' data-id='" . $row['id'] . "'>-</a>" . "<br>";
-
-                                echo "<a class='button-cart' href='../helpers/validate-cart.php' data-handler='del_product' data-id='" . $row['id'] . "'>trash</a>" . "<br>";
-                            }
-                            echo "<a class='button-cart--truncate' href='../helpers/validate-cart-truncate.php' data-handler='truncate_cart'>Limpar carrinho</a>";
-                        }else{
-                            echo "<h2>Seu carrinho esta vazio.</h2>";
-                        }
                         // Trazer soma total de produtos no carrinho
-                        
-                            $sql_total = $products->getSumTotalPricesInArray('"' . implode('","', array_keys($_SESSION['test'])) . '"');
-                            if($sql_total->num_rows > 0){
-                                $total = 0;
-                                while($row_values = $sql_total->fetch_assoc()){
-                                    $total = $total + ($row_values['valor'] * $_SESSION['test'][$row_values['id']]['qtd_product']);
-                                }
-                                echo 'Total: R$' . $total;
-                            }
-                }else{
-                    echo "<h2>Seu carrinho esta vazio.</h2>";
-                }
+                        $sql_total = $products->getSumTotalPricesInArray('"' . implode('","', array_keys($_SESSION['test'])) . '"');
 
+                        // verifica se o resultado existe
+                        if($sql_total->num_rows > 0):
+
+                            // coloco a variavel final com um valor default
+                            $total = 0;
+
+                            // Realizo um loop para todos os valores de produtos
+                            while($row_values = $sql_total->fetch_assoc()):
+                                $total = $total + ($row_values['valor'] * $_SESSION['test'][$row_values['id']]['qtd_product']);
+                            endwhile;
+                ?>
+                            <p>Total: R$ <?php echo $total ?></p>
+                            <br>
+                <?php
+                        endif;
+                    else:
+                ?>
+                        <h2>Seu carrinho esta vazio.</h2>
+                <?php
+                    endif;
                 ?>
                 <a href="<?php echo BASE_URL ?>views/index.php">Continuar comprando</a>
-            </div>
         </div>
     </main>
 </body>
